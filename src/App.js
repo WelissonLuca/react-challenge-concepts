4a12c75e39420beec3f0b5cc463c50728325c550
Welisson luca <welissonluca17@gmail.com>
2021-08-13 06:53:59 -0300

    feat: complete challenge

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+    const [repositories, setRepositories] = useState([]);
+		useEffect(() => {
+			api.get("repositories").then(response => {
+				setRepositories(response.data);
+			});
+		}, []);
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+			title: "LitaCoders ",
+			url: "https://github.com/LITA-CODERS/Lita-back-end-rest-api",
+			techs: ['Node.Js', 'Typescript'],
+    });
+    
+    setRepositories([ ...repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(repository => repository.id !== id))
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+		<div>
+			<ul data-testid="repository-list">
+				{repositories.map(repository => (
+					<li key={repository.id}>
+						{repository.title}
+						<button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+					</li>
+				))}
+			</ul>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+			<button onClick={handleAddRepository}>Adicionar</button>
+		</div>
+	);
 }
 
 export default App;
